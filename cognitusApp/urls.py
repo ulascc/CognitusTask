b15a78fcec311534f4291e43b1ffb1a0b6b88422
ulas.can2424@gmail.com <ulas.can2424@gmail.com>
2023-08-01 17:36:11 +0300

    postgresql configured and changes made in UI

--- cognitusApp/urls.py
+++ cognitusApp/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('dataworkspace/', DataView.as_view(), name='dataAPI'),
     path('dataPage/', TemplateView.as_view(template_name='index.html'), name='dataPage'),
+    path('predictPage/', TemplateView.as_view(template_name='predict.html'), name='predictPage'),
 ]
